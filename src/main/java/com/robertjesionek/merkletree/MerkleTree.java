5a0507826cc174bf158f369d8132143967da57a9
Robert Jesionek <yesionuk@gmail.com>
2017-04-24 22:08:39 +0200

    implemented addLeaf(s)

--- src/main/java/com/robertjesionek/merkletree/MerkleTree.java
+++ src/main/java/com/robertjesionek/merkletree/MerkleTree.java
@@ -1,10 +1,46 @@
 package com.robertjesionek.merkletree;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
 /**
  * Created by Jesion on 2017-04-24.
  */
 public class MerkleTree {
 
+    private boolean _hashLeafs = true;
 
+    private ArrayList<byte[]> _leafs = new ArrayList<byte[]>();
 
+    public MerkleTree(boolean hashLeafs) {
+        this._hashLeafs = hashLeafs;
+    }
+
+    public void reset() {
+        this._leafs.clear();
+    }
+
+    public void addLeaf(byte[] data) {
+        if (this._hashLeafs == true) {
+            data = this.sha256(data);
+        }
+        this._leafs.add(data);
+    }
+
+    public void addLeafs(ArrayList<byte[]> leafs) {
+        for (int i = 0; i < leafs.size(); i++) {
+           this.addLeaf(leafs.get(i));
+        }
+    }
+
+    public byte[] sha256(byte[] data) {
+        byte[] out = new byte[0];
+        try {
+            out = MessageDigest.getInstance("SHA256").digest(data);
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+        return out;
+    }
 }
--- src/test/java/com/robertjesionek/merkletree/MerkleTreeTest.java
+++ src/test/java/com/robertjesionek/merkletree/MerkleTreeTest.java
@@ -1,8 +1,13 @@
 package com.robertjesionek.merkletree;
 
 import org.bouncycastle.util.encoders.Hex;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Jesion on 2017-04-24.
@@ -10,13 +15,22 @@ import org.junit.Test;
 public class MerkleTreeTest {
 
     @Test
-    public void buildTest() {
+    public void buildTest() throws IOException, JSONException {
+        ArrayList<byte[]> leafs = new ArrayList<byte[]>();
+        JSONArray data = new TestResource("data.json").readObjectArray();
+        for (int i = 0; i < data.length (); i++) {
+            String leaf = (String) data.get(i);
+            byte[] leafBytes = Hex.decode(leaf);
+            leafs.add(leafBytes);
+            System.out.println("Decoded leaf at index " + i + " bytes: " + leafBytes.length);
+        }
+        MerkleTree tree = new MerkleTree(true);
+        tree.addLeafs(leafs);
 
-        String randomString = Hex.toHexString(DataGenerator.generate());
+
+
+
 
         Assert.assertTrue(true);
-
     }
-
-
 }
